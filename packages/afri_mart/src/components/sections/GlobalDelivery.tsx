2bca8bdf3bc6cc5083eca679dd7103c86129a60b
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-11-24 17:13:51 +0100

    some frontend update

---
+++ packages/afri_mart/src/components/sections/GlobalDelivery.tsx
@@ -0,0 +1,11 @@
+// components/GlobalDelivery.js
+
+export const GlobalDelivery = () => {
+    return (
+        <section className="bg-gray-200 py-8">
+            <div className="container mx-auto text-center">
+                {/* Add your global delivery content here */}
+            </div>
+        </section>
+    );
+};
