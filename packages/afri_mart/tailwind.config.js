7ab09a6c03ae7e2ba0b97593c8fc07895ba57252
akinbola247 <akinbolakehinde247@gmail.com>
2023-11-25 13:26:28 +0100

    added screen sizes

--- packages/afri_mart/tailwind.config.js
+++ packages/afri_mart/tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      xsm: { max: "320px" },
+      mobile: { max: "480px" },
+    },
     extend: {},
   },
   plugins: [],
