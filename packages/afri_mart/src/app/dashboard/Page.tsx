54bf37bbeb7768c17305d900593068a96a1e5267
akinbola247 <akinbolakehinde247@gmail.com>
2023-11-26 09:43:25 +0100

    updated dashboard pages

--- packages/afri_mart/src/app/dashboard/Page.tsx
+++ packages/afri_mart/src/app/dashboard/Page.tsx
@@ -1,7 +1,9 @@
+import AllPurchases from "./components/AllPurchases"
 export default function Home() {
     return (
         <div>
             <p>The dashboard content will be adaptive to the type of user</p>
+            <AllPurchases/>
         </div>
     )
 }
