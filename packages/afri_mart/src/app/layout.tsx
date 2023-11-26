648f0f5794fd1be23bc83018413daf85616b5e82
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-11-26 16:03:24 +0100

    merge resolve

--- packages/afri_mart/src/app/layout.tsx
+++ packages/afri_mart/src/app/layout.tsx
@@ -4,21 +4,16 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
 import { StarknetProvider } from '@/components/starknet-provider';
-<<<<<<< HEAD
-=======
-import { YourContextProvider } from '../context/YourContext';
-import { RegisteredContextProvider } from '../context/registeredContext';
->>>>>>> 464d566961aa3b0be8bc101278391f5eec7643d9
 import './globals.css';
 import { StarknetConfig, InjectedConnector } from '@starknet-react/core'
 import { Chain, goerli, mainnet } from '@starknet-react/chains';
 import { ProviderInterface, RpcProvider } from 'starknet';
 import clsx from 'clsx';
+import { RegisteredContextProvider } from '@/context/registeredContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-<<<<<<< HEAD
   const connectors = [
     new InjectedConnector({ options: { id: 'braavos' } }),
     new InjectedConnector({ options: { id: 'argentX' } }),
@@ -39,16 +34,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       >
         <StarknetProvider>{children}</StarknetProvider>
       </StarknetConfig>
-=======
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <RegisteredContextProvider>
-          <YourContextProvider>
-            <StarknetProvider>{children}</StarknetProvider>
-          </YourContextProvider>
-        </RegisteredContextProvider>
->>>>>>> 464d566961aa3b0be8bc101278391f5eec7643d9
       </body>
     </html>
   );
