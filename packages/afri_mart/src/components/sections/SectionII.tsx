daf72f33aaad6da36b30d306e92489634400b9d5
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-11-28 15:32:09 +0100

    added brands

--- packages/afri_mart/src/components/sections/SectionII.tsx
+++ packages/afri_mart/src/components/sections/SectionII.tsx
@@ -17,7 +17,7 @@ export const SectionII = () => {
                 <h1 className="font-serif text-4xl text-[var(--sand)]">About Us</h1>
                 <p 
                 className={clsx(
-                    "text-white mt-12 font-extralight",
+                    "text-white p-24 font-extralight",
                 )}
                 >AfriMart is an Afrocentric e-commerce store that provides a platform for talented  Africans to promote their cultural heritage. as a platform we are focused on sharing the richness of African culture to the global market , in form of NFTs, Artefacts, paintings and sculptures together with African fabrics. we are partnered with a global delivery company to ensure all purchases securely gets to customers.</p>
             </div>
