16cf96e6d6b2bb7cf1fd1d6ffb9e542aaba91d92
akinbola247 <akinbolakehinde247@gmail.com>
2023-11-30 00:35:32 +0100

    dashboard integration

--- packages/afri_mart/src/app/dash/components/AllPurchases.tsx
+++ packages/afri_mart/src/app/dash/components/AllPurchases.tsx
@@ -4,94 +4,100 @@ import {type ConnectedStarknetWindowObject, connect, disconnect } from '@argent/
 import { Contract, Provider, constants } from 'starknet'
 import { MarketPlaceAddr } from '../../../components/addresses';
 import marketplaceAbi from "@/ABI/marketPlace";
-// import { UsegetAlluserPurchase } from "@/utils/util";
-import {useGetAllUserPurchase, useGetOrder} from "@/utils/util";
+
+const AllPurchases =  ()  => {  
+const [allPurchase, setAllPurchase] = useState<any[]>([])
+const [allProductArray, setAllproductArray] = useState<any[]>([]);
+
+  const getAllpurchase = async () => {
+    const provider = new Provider({
+      rpc: {
+        nodeUrl: 'https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx',
+      },
+    });
+
+    try {
+      const connection = await connect({ modalMode: 'neverAsk', webWalletUrl: 'https://web.argent.xyz' });
+      const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider);
+      const allPurchaseData = await contract.getProductsBoughtByUser(
+        connection?.selectedAddress?.toString(),
+        connection?.selectedAddress?.toString()
+      );
+      setAllPurchase([...allPurchaseData]);
+    } catch (error : any) {
+  };
+}
 
 
-const AllPurchases = ()  => {
-  
-  const [address, setAddress] = useState('');
-  const [purchaseArray, setPurchaseArray] = useState<any[]>([]);
-  
-  
-  const {allPurchase, loading, error} = useGetAllUserPurchase();
-  const [newPurchaseAray, setNewpurchaseArray] = useState<any[]>([]);
-  const orderArray = useGetOrder(newPurchaseAray)
-  const [neworderArray, setneworderArray] = useState<any[]>();
-  
+  const GetOrder = async (args : any[]) => {
+    const provider = new Provider({
+      rpc: {
+        nodeUrl: 'https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx',
+      },
+    });
 
-  // }
+    try {
+      const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider);  
+      const promises = args.map(async (orderId) => {
+        let nextId = await contract.getOrderDetails(orderId.toString());
+        return Number(nextId.itemID);
+      });
+      const results = await Promise.all(promises);
+      return results
+    } catch (error: any) {}
+  };
 
-  // const getProductInfo = async () =>{
-  //   const provider = new Provider({  
-  //     rpc: {
-  //        nodeUrl: "https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx"
-  //      }
-  //    })
-  //      try {
-  //      const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider)
-  //      const productinformation = await contract.getProductDetails(itemid.toString());
-  //         console.log(productinformation) 
-  //      } catch (error : any) {
+  const GetItem = async (args : number[] | undefined) => {
+    const provider = new Provider({
+      rpc: {
+        nodeUrl: 'https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx',
+      },
+    });
 
-  //      }
-  // }
-  
-  // const fetchAll = () =>{
-  //    getAlluserPurchase().then(()=>{getOrderInfo()})
-  // }
-  
+    try {
+      const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider);  
+      //@ts-ignore
+      const promises = args.map(async (productId) => {
+        let productdetail = await contract.getProductDetails(productId.toString());
+        return productdetail;
+      });
+      const results = await Promise.all(promises);
+      return results
+    } catch (error: any) {}
+  };
+
+  function hexToReadableText(hexString : any) {
+    const bytes = Buffer.from(hexString, 'hex'); 
+    const text = new TextDecoder('utf-8').decode(bytes);
+    return text;
+  }
+
+useEffect(() => {
+  getAllpurchase()
+}, [])
+
   useEffect(() => {
-    // const connectToStarknet = async() => {
-    //   // const connection = await connect({ modalMode: "neverAsk", webWalletUrl: "https://web.argent.xyz" })
-    //   if(connection && connection.isConnected) {
-    //     setAddress(connection.selectedAddress)
-    //   }
-    // }
-    // connectToStarknet()
-    // fetchAll();
-
-    // setPurchaseArray([returned])
-   
-    // console.log(orderArray)
-    if(allPurchase){
-      const numbersArray = allPurchase.map((bigintValue) => Number(bigintValue));
-      setNewpurchaseArray(numbersArray)
+    if(allPurchase.length > 0){
+      GetOrder(allPurchase).then((orderidsArray)=>{
+        // console.log('order id array collected', orderidsArray)
+        GetItem(orderidsArray).then((products)=>{
+          // console.log('products obtained array',products)
+          //@ts-ignore
+          setAllproductArray(products)
+        })
+      }).catch((error)=>{
+          console.log(error)
+      })
     }
-    if(orderArray){
-      const resolve = async () => {
-        let result  = await orderArray;
-        console.log(result)
-        // setneworderArray(result)
-      }
-      resolve()
-    }
-  },[allPurchase,orderArray])  
-
-
-  const data = [
-    {
-      title : 'Ashoke Material',
-      amount : 150,
-      quantity : 5      
-    },  {
-      title : 'Edo Material',
-      amount : 100,
-      quantity : 2      
-    },  {
-      title : 'Akure Material',
-      amount : 550,
-      quantity : 3      
-    },
-  ]
+  }, [allPurchase]); 
   
-
-
   return    ( <div className="smx:border-2 lmx:border-2 lmx:p-6 smx:p-4 smx:border-black lmx:border-black mx-auto w-[800px] smx:w-[80%] lmx:w-[90%] h-[80%] p-6 mt-2">   
       
-      {neworderArray?.map(async (item:number,index : number) => {    
+      {allProductArray?.map( (item:any,index : number) => {  
+          let productname =  hexToReadableText(item.name.toString(16)) 
+          let productprice = Number(item.price)/1e18
          return ( <div key={index} className="w-[20%] space-y-10">          
-         <Puchasecard title='null' amount={0} quantity={0} />
+         <Puchasecard title={productname} amount={productprice} quantity={0} />
        </div> )             
     })}
 </div>)
--- packages/afri_mart/src/app/dash/components/SoldItems.tsx
+++ packages/afri_mart/src/app/dash/components/SoldItems.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Soldcard from "./Soldcard";
+import {type ConnectedStarknetWindowObject, connect, disconnect } from '@argent/get-starknet'
+import { Contract, Provider, constants } from 'starknet'
+import { MarketPlaceAddr } from '../../../components/addresses';
+import marketplaceAbi from "@/ABI/marketPlace";
 
 const SoldItems = () => {
+const [allSold, setAllSold] = useState<any[]>([])
+const [allProductSold, setAllproductSold] = useState<any[]>([]);
+
+
+  
+  const getAllsolditem = async () => {
+    const provider = new Provider({
+      rpc: {
+        nodeUrl: 'https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx',
+      },
+    });
+
+    try {
+      const connection = await connect({ modalMode: 'neverAsk', webWalletUrl: 'https://web.argent.xyz' });
+      const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider);
+      const allsoldData = await contract.getItemsSold(
+        connection?.selectedAddress?.toString()
+      );
+      // setAllSold([...allsoldData]);
+      console.log(allsoldData);
+    } catch (error : any) {
+      console.log(error)
+  };
+}
+
+const GetProductSold = async (args : number[] | undefined) => {
+  const provider = new Provider({
+    rpc: {
+      nodeUrl: 'https://starknet-goerli.g.alchemy.com/v2/mIOPEtzf3iXMb8KvqwdIvXbKmrtyorYx',
+    },
+  });
+
+  try {
+    const contract = new Contract(marketplaceAbi, MarketPlaceAddr(), provider);  
+    //@ts-ignore
+    const promises = args.map(async (productId) => {
+      if(productId != 0x0){
+        let productdetail = await contract.getProductDetails(productId.toString());
+        return productdetail;
+      }
+    });
+    const results = await Promise.all(promises);
+    return results
+  } catch (error: any) {
+    console.log(error)
+  }
+};
+  
+useEffect(() => {
+  getAllsolditem()
+}, [])
+
+useEffect(() => {
+  if(allSold.length > 0){
+    GetProductSold(allSold).then((products)=>{
+      console.log('sold product collected', products)
+      //@ts-ignore
+      setAllproductSold(products)
+    }).catch((error)=>{
+        console.log(error)
+    })
+  }
+}, [allSold]); 
+
   const data = [
     {
       title : 'Ofa Material',
