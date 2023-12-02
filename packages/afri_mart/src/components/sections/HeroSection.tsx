91c6369d9a38fda53c8a40f558d883066f67c760
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-12-02 08:19:35 +0100

    frontend mvp

--- packages/afri_mart/src/components/sections/HeroSection.tsx
+++ packages/afri_mart/src/components/sections/HeroSection.tsx
@@ -8,19 +8,11 @@ import { Variant, motion, useAnimation, useInView } from 'framer-motion';
 import { AnimatedText } from "../animations/animations";
 import { useEffect, useRef } from "react";
 import { MainAnimation } from "../animations/mainanimation";
-import img1 from "../../../public/image/anims/asambe.jpeg";
-import img2 from "../../../public/image/anims/burundi.jpeg";
-import img3 from "../../../public/image/anims/honey.jpg";
-import img4 from "../../../public/image/anims/jameela.jpg";
-import img5 from "../../../public/image/anims/1964.png";
-import img6 from "../../../public/image/anims/ganda.png";
-import img7 from "../../../public/image/anims/toys.jpg";
-import img8 from "../../../public/image/anims/threads.png";
-import img9 from "../../../public/image/anims/protea.png";
-import img10 from "../../../public/image/anims/nuerta.jpg";
+
 
 import Image from "next/image";
 import Link from "next/link";
+import { BackgroundImageAnim } from "../animations/background-image-anim";
 
 export const HeroSection = () => {
     return (
@@ -45,73 +37,5 @@ export const HeroSection = () => {
             </div>
             <BackgroundImageAnim />
         </Container>
-    );
-};
-
-const BackgroundImageAnim = () => {
-    return (
-        <div className="flex absolute h-[90vh] overflow-clip opacity-40 top-0 w-full z-0">
-            <motion.div
-                className="flex flex-col w-64  space-y-20 top-20 absolute bg-red"
-                animate={{
-                    y: [400, 800],
-                    transition: {
-                        duration: 50,
-                        repeat: Infinity,
-                        repeatType: 'reverse'
-                    }
-                }}
-            >
-                <Image src={img1} alt={"AsambeZ"} className="transition" />
-                <Image src={img2} alt={"AsambeZ"} />
-                <Image src={img3} alt={"AsambeZ"} />
-                <Image src={img4} alt={"AsambeZ"} />
-            </motion.div>
-            <motion.div
-                className="flex flex-col w-64 right-0 space-y-40 absolute top-0"
-                animate={{
-                    y: [400, 800],
-                    transition: {
-                        duration: 50,
-                        repeat: Infinity,
-                        repeatType: 'reverse'
-                    }
-                }}
-            >
-                <Image src={img2} alt={"AsambeZ"} />
-                <Image src={img5} alt={"AsambeZ"} />
-                <Image src={img7} alt={"AsambeZ"} />
-                <Image src={img3} alt={"AsambeZ"} />
-            </motion.div>
-            <motion.div
-                className="w-64 right-96 bottom-0 space-y-40 absolute"
-                animate={{
-                    y: [400, -800],
-                    transition: {
-                        duration: 50,
-                        repeat: Infinity,
-                        repeatType: 'reverse'
-                    }
-                }}>
-                <Image src={img3} alt={"AsambeZ"} />
-                <Image src={img1} alt={"AsambeZ"} />
-                <Image src={img8} alt={"AsambeZ"} />
-                <Image src={img2} alt={"AsambeZ"} />
-            </motion.div>
-            <motion.div className="w-80 left-20 space-y-40 absolute bottom-0"
-                animate={{
-                    y: [400, -800],
-                    transition: {
-                        duration: 50,
-                        repeat: Infinity,
-                        repeatType: 'reverse'
-                    }
-                }}>
-                <Image src={img7} alt={"AsambeZ"} />
-                <Image src={img8} alt={"AsambeZ"} />
-                <Image src={img9} alt={"AsambeZ"} />
-                <Image src={img10} alt={"AsambeZ"} />
-            </motion.div>
-        </div>
     );
 };
